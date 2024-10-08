12d51f8ed091eb19b8dcbcc5d8e6af5d40662122
Fábio Souza <fabiocdfmedeiros@hotmail.com>
2024-10-08 00:40:14 -0300

    Código de aprendizado - Learning Code
    Código "escola" (school hobby code) - made only to learn. Just, don't look the mistakes and (you know) bear with me, i'm a newbie on OpenGL and C++.

--- Project2/Origem.cpp
+++ Project2/Origem.cpp
@@ -1,5 +1,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+
+
+void framebuffer_size_callback(GLFWwindow* window, int widht, int height);
 
 
 int main()
@@ -10,7 +15,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FOWARD_COMPAT, GL_TRUE);
-    
+
     GLFWwindow* window;
    
     /* Create a windowed mode window and its OpenGL context */
@@ -23,12 +28,25 @@ int main()
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    gladLoadGL();
+
+    glViewport(0, 0, 640, 480);
+
+    //glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+    
+   // glfwSwapBuffers(window); - Todos os problemas de código, com viewport, com glClearColor e glClear
+   // eram causados pelo fato de "SwapBuffers" não estar no looping de renderização.
+  //  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); 
+   
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-       // glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -37,6 +55,19 @@ int main()
         glfwPollEvents();
     }
 
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+
+
     glfwTerminate();
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int widht, int height)
+{
+    glViewport(0, 0, widht, height);
 }
